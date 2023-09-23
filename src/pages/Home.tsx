52fb16733d06dbaae267323e7741d8122816a610
Nikita <nekitrus92@yandex.ru>
2023-09-23 22:39:19 +0300

    first com

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,30 @@
-import { FC, useState } from "react"
-import { Space, Row } from "antd"
+import { FC, useEffect, useState } from "react"
+import { Space, Row, Spin } from "antd"
+import { useFetchPostsQuery } from "../API/jsonApi"
 import { useSearchedPosts } from "hooks/useSearchedPosts"
+import { useActions } from "../hooks/useActions"
 import PostModal from "components/PostModal"
 import Posts from "components/Posts"
 import UIAlert from "components/UIAlert"
+import css from "../styles/Home.module.css"
 
 const Home: FC = () => {
+  const [sort] = useState<string>("")
   const [search] = useState<string>("")
+
+  const { setPosts } = useActions()
+
+  const { data: posts_data, isLoading: posts_loading } =
+    useFetchPostsQuery(sort)
+
   const posts = useSearchedPosts(search)
+
+  useEffect(() => {
+    setPosts(posts_data)
+  }, [setPosts, posts_data])
+
+  if (posts_loading) return <Spin className={css.center} size="large" />
+
   return (
     <>
       {posts ? (
